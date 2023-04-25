fe438b5e0846874236a797dba83cc9fa691d445e
Tudor Pascu <pascutudoralexandru@gmail.com>
2023-04-25 10:49:48 +0300

    First Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,91 @@
+<template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" v-model="searchQuery" 
+      @input="getSearchResults"
+      placeholder="Search for a city or state"
+      class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary
+      focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+
+      <ul 
+      class="absolute bg-weather-secondary text-white
+      w-full shadow-md py-2 px-1 top-[66px]"
+      v-if="mapSearchResults"
+      >
+
+      <p v-if="searchError">
+        Sorry, something went wrong, please try again.
+      </p>
+
+      <p v-if="!serveError && mapSearchResults.length === 0"
+      >
+      No results match your query, try a different term.
+    </p>
+
+      <Temple v-else>
+        <li v-for="searchResult in mapSearchResults" :key="searchResult.id"
+        class="py-2 cursor-pointer"
+        @click="previewCity(searchResult)"
+        >
+        {{ searchResult.place_name }}
+            </li>
+      </Temple>
+    </ul>
+    </div>
+    <div class="flex flex-col gap-4">
+      <Suspense>
+        <CityList/>
+        <template #fallback>
+         <p>Loading...</p>
+        </template>
+      </Suspense>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from "axios";
+import { useRouter } from "vue-router";
+import CityList from '../components/CityList.vue';
+
+const router = useRouter();
+const previewCity = (searchResult) => {
+  console.log(searchResult);
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: 'cityView',
+    params: {state: state.replaceAll(" ", ""), city: city},
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
+
+const mapBoxAPIKey = "pk.eyJ1IjoicGFzY3V0dWRvcjE5OTkiLCJhIjoiY2xldmMzcWk5MXl6ajNxcDRlMzdsYzU3eSJ9.AkzxtaSH_WVX-jrLt8B2FA";
+const searchQuery = ref("")
+const queryTimeout = ref(null);
+const mapSearchResults = ref(null);
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if(searchQuery.value !=="") {
+      try {
+        const result = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapBoxAPIKey}&types=place`
+        );
+      mapSearchResults.value = result.data.features;
+      } catch {
+        searchError.value = true;
+      }
+     
+      return;
+    }
+    mapSearchResults.value = null;
+  }, 300);
+}
+</script>
